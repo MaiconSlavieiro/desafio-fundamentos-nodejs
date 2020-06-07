29eb72fbdbf498314d119a4f0450e76448f87744
Maicon Slavieiro <maiconslavieiro@outlook.com>
2020-06-07 15:47:23 -0300

    Desafio: Fundamentos Node.js

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,29 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    if (!title) {
+      throw Error('A title is requerid to create a transaction');
+    }
+    if (!type) {
+      throw Error('A type is requerid to create a transaction');
+    }
+    if (!value) {
+      throw Error('A value is requerid to create a transaction');
+    }
+    if (type === 'outcome' && value > balance.total) {
+      throw Error(
+        `A transaction of type 'outcome' can't be menor of the total`,
+      );
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
